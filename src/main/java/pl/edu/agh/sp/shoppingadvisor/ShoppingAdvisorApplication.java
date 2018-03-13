52a77e59fd79c1612dc7f08ebab128004c6cd2cf
Mateusz Kuźmik <mateusz.kuzmik1@gmail.com>
2018-03-13 15:24:54 +0100

    Initial commit

---
+++ src/main/java/pl/edu/agh/sp/shoppingadvisor/ShoppingAdvisorApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.sp.shoppingadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingAdvisorApplication.class, args);
+	}
+}
