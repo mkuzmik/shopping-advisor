39a0aad618c1e6f4f3d2f36357015c788b1659af
Mateusz Kuźmik <mateusz.kuzmik1@gmail.com>
2018-05-08 22:57:06 +0200

    Basic classification endpoint implemented

--- flask-ml/flask-ml.py
+++ flask-ml/flask-ml.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from classification import *
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+    def post(self):
+        training_data = request.json['training_data']
+        test_data = request.json['test_data']
+        # print_most_correlated_terms(training_data)
+        return classify(training_data, test_data).ravel().tolist()
 
 api.add_resource(HelloWorld, '/')
 
