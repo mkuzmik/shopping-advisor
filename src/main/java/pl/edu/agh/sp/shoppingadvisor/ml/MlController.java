8b81e1065339ebf92497cbfc4d4789695894a3a7
Mateusz Kuźmik <mateusz.kuzmik1@gmail.com>
2018-05-15 22:00:31 +0200

    Recommendation endpoint created

--- src/main/java/pl/edu/agh/sp/shoppingadvisor/ml/MlController.java
+++ src/main/java/pl/edu/agh/sp/shoppingadvisor/ml/MlController.java
@@ -1,19 +1,32 @@
 package pl.edu.agh.sp.shoppingadvisor.ml;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vavr.control.Try;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.agh.sp.shoppingadvisor.scraper.AllegroScraper;
+
+import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/learning")
 public class MlController {
 
     @Autowired
     private FlaskService flaskService;
 
-    @RequestMapping(value = "/flask", method = RequestMethod.GET)
-    public Hello foo(){
-        return flaskService.getHello();
+    @Autowired
+    private AllegroScraper allegroScraper;
+
+    @PostMapping
+    public RecommendationOutput learn(@RequestBody RecommendationInput recommendationInput){
+        recommendationInput.setTraining_offers(recommendationInput.getTraining_offers().stream().peek(offer -> {
+            String description = Try.of(() -> String.join(" ", allegroScraper.scrapDescription(offer.getUrl()))).getOrElse("");
+            offer.setDescription(description);
+        }).collect(Collectors.toList()));
+        recommendationInput.setTest_offers(recommendationInput.getTest_offers().stream().peek(offer -> {
+            String description = Try.of(() -> String.join(" ", allegroScraper.scrapDescription(offer.getUrl()))).getOrElse("");
+            offer.setDescription(description);
+        }).collect(Collectors.toList()));
+        return flaskService.recommend(recommendationInput);
     }
 }
