1643b9a41381752d808bae117c9c3e8f9bf38c56
Mateusz Kuźmik <mateusz.kuzmik1@gmail.com>
2018-05-16 10:24:30 +0200

    Logistic regression instead of bayes

--- flask-ml/classification.py
+++ flask-ml/classification.py
@@ -6,6 +6,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+
 
 def classify(training_data, test_data):
     df = pd.DataFrame(training_data)
@@ -18,7 +21,7 @@ def classify(training_data, test_data):
     tfidf_transformer = TfidfTransformer()
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
-    clf = MultinomialNB().fit(X_train_tfidf, y_train)
+    clf = LogisticRegression(random_state=0).fit(X_train_tfidf, y_train)
     return clf.predict_proba(count_vect.transform(map(lambda data: data['description'], test_data)))
 
 def build_features(dataset):
